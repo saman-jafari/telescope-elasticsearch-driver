70b0662d95161c90f2115aaf6e5c33e3ba5f0c25
Saman-jafari <isaimon@me.com>
2023-06-29 22:16:54 +0200

    fix: minor correction

--- config/config.php
+++ config/config.php
@@ -4,5 +4,5 @@ return [
     'host' => env('ELASTICSEARCH_HOST', 'elasticsearch:9200'),
     'username' => env('ELASTICSEARCH_USERNAME', ''),
     'password' => env('ELASTICSEARCH_PASSWORD', ''),
-    'index' => env('ELASTICSEARCH_INDEX', 'telescope_bl'),
+    'index' => env('ELASTICSEARCH_INDEX', 'telescope'),
 ];

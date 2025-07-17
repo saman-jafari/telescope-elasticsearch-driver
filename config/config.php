c0f91f04eb02a18ac7c16b0d940176cacfc08847
Mahmoud Awadeen <mahmoud.awadeen@gmail.com>
2025-07-17 18:51:50 +0300

    [v2.0.4] Add index suffix format

--- config/config.php
+++ config/config.php
@@ -5,4 +5,5 @@ return [
     'username' => env('TELESCOPE_ELASTICSEARCH_USERNAME', ''),
     'password' => env('TELESCOPE_ELASTICSEARCH_PASSWORD', ''),
     'index'    => env('TELESCOPE_ELASTICSEARCH_INDEX', 'telescope'),
+    'index_suffix_format' => env('TELESCOPE_ELASTICSEARCH_INDEX_SUFFIX_FORMAT'), // e.g. 'Y.m.d', 'Y.m', 'Y.m.d.H', etc.
 ];
